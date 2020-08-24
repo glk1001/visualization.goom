3b09715eb9a173c44987323eb9d633b616d6df07
glk1001 <gregg.kay@gmail.com>
2020-08-24 23:13:06 +1000

    convolve_fx.c: remove unused code from previous removal of motif.

--- lib/goom/src/convolve_fx.c
+++ lib/goom/src/convolve_fx.c
@@ -9,44 +9,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define NB_THETA 512
-
-#define MAX 2.0f
-
 typedef struct _CONV_DATA {
   PluginParam light;
   PluginParam factor_adj_p;
   PluginParam factor_p;
   PluginParameters params;
-
-  /* rotozoom */
-  int theta;
-  float ftheta;
-  int h_sin[NB_THETA];
-  int h_cos[NB_THETA];
-  int h_height;
-  float visibility;
 } ConvData;
-
-/* init rotozoom tables */
-static void compute_tables(VisualFX* _this, PluginInfo* info)
-{
-  ConvData* data = (ConvData*)_this->fx_data;
-
-  if (data->h_height == info->screen.height) {
-    return;
-  }
-
-  const double screen_coef = 2.0 * 300.0 / (double)info->screen.height;
-  data->h_height = info->screen.height;
-
-  for (int i = 0; i < NB_THETA; i++) {
-    const double radian = 2 * i * M_PI / NB_THETA;
-    const double h = (0.2 + cos(radian) / 15.0 * sin(radian * 2.0 + 12.123)) * screen_coef;
-    data->h_cos[i] = 0x10000 * (-h * cos(radian) * cos(radian));
-    data->h_sin[i] = 0x10000 * (h * sin(radian + 1.57) * sin(radian));
-  }
-}
 
 static void convolve_init(VisualFX* _this, PluginInfo* info)
 {
@@ -73,12 +41,6 @@ static void convolve_init(VisualFX* _this, PluginInfo* info)
   data->params.params[3] = &data->factor_p;
   data->params.params[4] = 0;
 
-  /* init rotozoom tables */
-  compute_tables(_this, info);
-  data->theta = 0;
-  data->ftheta = 0.0;
-  data->visibility = 1.0;
-
   _this->params = &data->params;
 }
 
@@ -89,8 +51,7 @@ static void convolve_free(VisualFX* _this)
   free(data);
 }
 
-static void create_output_with_brightness(VisualFX* _this, Pixel* src, Pixel* dest,
-                                          PluginInfo* info, unsigned int iff)
+static void create_output_with_brightness(Pixel* src, Pixel* dest, PluginInfo* info, unsigned int iff)
 {
   int i = 0; //info->screen.height * info->screen.width - 1;
   for (int y = 0; y < info->screen.height; y++) {
@@ -108,8 +69,6 @@ static void create_output_with_brightness(VisualFX* _this, Pixel* src, Pixel* de
       i++;
     }
   }
-
-  compute_tables(_this, info);
 }
 
 /*#include <stdint.h>
@@ -127,7 +86,6 @@ static void convolve_apply(VisualFX* _this, Pixel* src, Pixel* dest, PluginInfo*
   const float ff = (FVAL(data->factor_p) * FVAL(data->factor_adj_p) + FVAL(data->light)) / 100.0f;
   const unsigned int iff = (unsigned int)(ff * 256);
   {
-    const double fcycle = (double)info->cycle;
     const float INCREASE_RATE = 1.5;
     const float DECAY_RATE = 0.955;
     if (FVAL(info->sound.last_goom_p) > 0.8) {
@@ -135,28 +93,13 @@ static void convolve_apply(VisualFX* _this, Pixel* src, Pixel* dest, PluginInfo*
     }
     FVAL(data->factor_p) *= DECAY_RATE;
 
-    float rotate_param = FVAL(info->sound.last_goom_p);
-    if (rotate_param < 0.0) {
-      rotate_param = 0.0;
-    }
-    rotate_param += FVAL(info->sound.goom_power_p);
-
-    const float rotate_coef = 4.0 + FVAL(info->sound.goom_power_p) * 6.0;
-    data->ftheta = (data->ftheta + rotate_coef * sin(rotate_param * 6.3));
-    data->theta = ((unsigned int)data->ftheta) % NB_THETA;
-    data->visibility = (cos(fcycle * 0.001 + 1.5) * sin(fcycle * 0.008) +
-                        cos(fcycle * 0.011 + 5.0) - 0.8 + info->sound.speedvar) *
-                       1.5;
-    if (data->visibility < 0.0) {
-      data->visibility = 0.0;
-    }
     data->factor_p.change_listener(&data->factor_p);
   }
 
   if ((ff > 0.98f) && (ff < 1.02f)) {
     memcpy(dest, src, (size_t)info->screen.size * sizeof(Pixel));
   } else {
-    create_output_with_brightness(_this, src, dest, info, iff);
+    create_output_with_brightness(src, dest, info, iff);
   }
   /*
    //   Benching suite...
