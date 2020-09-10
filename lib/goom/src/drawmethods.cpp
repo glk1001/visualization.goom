4c6120cb802e5baf13562f24024563399ef8e7ff
glk1001 <gregg.kay@gmail.com>
2020-09-11 00:25:11 +1000

    Fixed bug with wu anti-alias.

--- lib/goom/src/drawmethods.cpp
+++ lib/goom/src/drawmethods.cpp
@@ -1,5 +1,7 @@
 #include "drawmethods.h"
 
+#include "colorutils.h"
+
 #undef NDEBUG
 #include <cassert>
 #include <cmath>
@@ -46,11 +48,68 @@ void draw_line(const size_t n,
   draw_wuline(n, buffs, cols, x1, y1, x2, y2, screenx, screeny);
 }
 
-static void WuDrawLine(float x0,
-                       float y0,
-                       float x1,
-                       float y1,
-                       const std::function<void(int x, int y, float brightess)>& plot)
+using PlotFunc = const std::function<void(int x, int y, float brightess)>;
+static void WuDrawLine(float x0, float y0, float x1, float y1, const PlotFunc&);
+
+inline void draw_pixels(const size_t n,
+                        Pixel* buffs[],
+                        const std::vector<Pixel>& cols,
+                        const int pos)
+{
+  for (size_t i = 0; i < n; i++)
+  {
+    Pixel* const p = &(buffs[i][pos]);
+    *p = getColorAdd(*p, getHalfIntensityColor(cols[i]));
+  }
+}
+
+static void draw_wuline(const size_t n,
+                        Pixel* buffs[],
+                        const std::vector<Pixel>& cols,
+                        const int x1,
+                        const int y1,
+                        const int x2,
+                        const int y2,
+                        const uint16_t screenx,
+                        const uint16_t screeny)
+{
+  if ((y1 < 0) || (y2 < 0) || (x1 < 0) || (x2 < 0) || (uint16_t(y1) >= screeny) ||
+      (uint16_t(y2) >= screeny) || (uint16_t(x1) >= screenx) || (uint16_t(x2) >= screenx))
+  {
+    return;
+  }
+
+  assert(n == cols.size());
+  std::vector<Pixel> colors = cols;
+  auto plot = [&](const int x, const int y, const float brightness) -> void {
+    if (uint16_t(x) >= screenx || uint16_t(y) >= screeny)
+    {
+      return;
+    }
+    if (brightness < 0.001)
+    {
+      return;
+    }
+    const int pos = y * static_cast<int>(screenx) + x;
+    if (brightness >= 0.999)
+    {
+      draw_pixels(n, buffs, cols, pos);
+    }
+    else
+    {
+      for (size_t i = 0; i < cols.size(); i++)
+      {
+        colors[i] = getBrighterColor(brightness, cols[i]);
+      }
+      draw_pixels(n, buffs, colors, pos);
+    }
+  };
+
+  WuDrawLine(x1, y1, x2, y2, plot);
+}
+
+// The Wu anti-aliased draw line.
+static void WuDrawLine(float x0, float y0, float x1, float y1, const PlotFunc& plot)
 {
   const auto ipart = [](const float x) -> int { return static_cast<int>(std::floor(x)); };
   const auto round = [](const float x) -> float { return std::round(x); };
@@ -131,98 +190,4 @@ static void WuDrawLine(float x0,
       intery += gradient;
     }
   }
-}
-
-inline uint8_t brighten(const uint32_t br, const unsigned char c)
-{
-  return static_cast<uint8_t>((br * uint32_t(c)) >> 8);
-}
-
-inline Pixel getColor(const float brightness, const Pixel& color)
-{
-  assert(brightness >= 0.0 && brightness <= 1.0);
-  const uint32_t br = uint32_t(brightness * 255);
-  Pixel c;
-  c.channels.r = brighten(br, color.channels.r);
-  c.channels.g = brighten(br, color.channels.g);
-  c.channels.b = brighten(br, color.channels.b);
-  c.channels.a = brighten(br, color.channels.a);
-  return c;
-}
-
-inline uint8_t colorAdd(const unsigned char c1, const unsigned char c2)
-{
-  uint32_t cadd = uint32_t(c1) + (uint32_t(c2) >> 1);
-  if (cadd > 255)
-  {
-    cadd = 255;
-  }
-  return static_cast<uint8_t>(cadd);
-}
-
-inline Pixel getColorAdd(const Pixel& color1, const Pixel& color2)
-{
-  Pixel c;
-  c.channels.r = colorAdd(color1.channels.r, color2.channels.r);
-  c.channels.g = colorAdd(color1.channels.g, color2.channels.g);
-  c.channels.b = colorAdd(color1.channels.b, color2.channels.b);
-  c.channels.a = colorAdd(color1.channels.a, color2.channels.a);
-  return c;
-}
-
-inline void draw_pixels(const size_t n,
-                        Pixel* buffs[],
-                        const std::vector<Pixel>& cols,
-                        const int pos)
-{
-  for (size_t i = 0; i < n; i++)
-  {
-    Pixel* const p = &(buffs[i][pos]);
-    *p = getColorAdd(*p, cols[i]);
-  }
-}
-
-static void draw_wuline(const size_t n,
-                        Pixel* buffs[],
-                        const std::vector<Pixel>& cols,
-                        const int x1,
-                        const int y1,
-                        const int x2,
-                        const int y2,
-                        const uint16_t screenx,
-                        const uint16_t screeny)
-{
-  if ((y1 < 0) || (y2 < 0) || (x1 < 0) || (x2 < 0) || (uint16_t(y1) >= screeny) ||
-      (uint16_t(y2) >= screeny) || (uint16_t(x1) >= screenx) || (uint16_t(x2) >= screenx))
-  {
-    return;
-  }
-
-  assert(n == cols.size());
-  const std::vector<Pixel> colors = cols;
-  auto plot = [&](int x, int y, float brightness) -> void {
-    if (uint16_t(x) >= screenx || uint16_t(y) >= screeny)
-    {
-      return;
-    }
-    if (brightness < 0.001)
-    {
-      return;
-    }
-    const int pos = y * int(screenx) + x;
-    if (brightness >= 0.999)
-    {
-      draw_pixels(n, buffs, cols, pos);
-    }
-    else
-    {
-      for (auto c : colors)
-      {
-        c = getColor(brightness, c);
-      }
-      draw_pixels(n, buffs, colors, pos);
-    }
-  };
-
-  WuDrawLine(x1, y1, x2, y2, plot);
 }
